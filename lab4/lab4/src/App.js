0f957758fe6f01cb0e82aa26ce517122ba61f0ae
ashmashaik <110445152+ashmashaik@users.noreply.github.com>
2023-04-01 16:49:12 +0530

    Add files via upload

---
+++ lab4/lab4/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Ex4 from "./components/Ex4";
+function App() {
+  return (
+    <div className="App">
+     <Ex4/>
+    </div>
+  );
+}
+
+export default App;
