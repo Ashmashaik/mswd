abf03d9a72ee6cb4f2953c71fdb5061a33cb92d0
ashmashaik <110445152+ashmashaik@users.noreply.github.com>
2023-04-01 16:45:49 +0530

    Add files via upload

---
+++ lab3/lab3/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import EX3 from "./components/Ex3"
+
+function App() {
+  return (
+    <div className="App">
+<EX3/>
+    </div>
+    
+  );
+}
+
+export default App;
